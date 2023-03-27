db7240611de843162495b7f38de33ca470138151
Raphael Torsu <torsur699@gmail.com>
2023-03-27 14:33:20 +0000

    Add validation library

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Post, Req, Body } from "@nestjs/common";
+import { Request } from "express";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto/auth.dto";
 
 @Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post("signup")
-  signup() {
-    this.authService.signUp();
+  signup(@Body('input') dto: AuthDto) {
+    console.log({dto})
+    return this.authService.signUp();
   }
 
   @Post("login")
-  login() {
-    this.authService.login();
+  login(@Req() req: Request) {
+    console.log(req)
+    return this.authService.login();
   }
 }
